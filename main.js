c2cf9cf9e45e69e9a79d855c85de7ddaa7de3dd8
Martina Gelnerová <martina.gelner.cz@gmail.com>
2022-04-05 20:43:39 +0200

    řešení

--- main.js
+++ main.js
@@ -9,3 +9,24 @@ která vypíše kalorickou hodnotu potraviny.
 BONUS: počítej a zobrazuj celkový počet sněděných kalorií.
 Tj. při klikání na obrázky se postupně sčítají kalorie.
 */
+const obrazky = document.querySelectorAll('img');
+
+obrazky.forEach((obrazek) => {
+  obrazek.addEventListener('click', zobrazKalorie);
+});
+
+function zobrazKalorie(udalost) {
+  let obrazek = udalost.target;
+
+  let kalorie = obrazek.dataset.kalorie;
+  let zeme = obrazek.dataset.zemePuvodu;
+
+  let soucetKaloriiElement = document.querySelector('#soucet-kalorii');
+
+  document.querySelector('#vysledek').textContent = `Snědl jsi ${kalorie} kcal/100g a země původu je ${zeme}.`;
+  soucetKaloriiElement.textContent = parseInt(soucetKaloriiElement.textContent) + parseInt(obrazek.dataset.kalorie);
+
+  let snedeno = document.querySelector('#snedeno');
+  obrazek.removeEventListener('click', zobrazKalorie);
+  snedeno.appendChild(obrazek);
+}
